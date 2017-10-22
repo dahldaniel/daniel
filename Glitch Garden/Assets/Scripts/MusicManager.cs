4927c6f60384854cfd49e2a013d570f305ec4fec
Daniel Dahl <daniel_dahl@live.com>
2017-10-22 08:05:28 +0200

    Stage all files

---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] LevelMusic;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Start()
+	{
+		audioSource = this.GetComponent<AudioSource>();
+	}
+
+	 void OnLevelWasLoaded(int level) // Dette er en Unity method
+	{
+		AudioClip thisLevelMusic = LevelMusic[level];
+
+		if (thisLevelMusic)
+		{
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+
+
+}
