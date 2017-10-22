8a9cdeb8fac1ec7eb6901a4673ea51bd56f3c494
Daniel Dahl <daniel_dahl@live.com>
2017-10-22 07:17:56 +0200

    All files

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public float autoLoadNextLevel;
+
+	public void LoadLevel(string name)
+	{
+		Application.LoadLevel (name);
+	}
+
+	public void LoadNextLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		Invoke ("LoadNextLevel", autoLoadNextLevel);
+	}
+
+		
+}
